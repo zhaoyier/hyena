1d9c117d18f2cc4835d40d7fed858801f80bf31d
zhaoyier <zjw_0911@126.com>
2016-01-08 14:38:48 +0800

    update

--- game-client/Assets/Script/LoginScript/LoginService.cs
+++ game-client/Assets/Script/LoginScript/LoginService.cs
@@ -1,11 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using SimpleJson;
+using UnityEngine;
 using System.Collections;
+using Pomelo.DotNetClient;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
 
 public class LoginService : MonoBehaviour {
+	private static PomeloClient _pclient = null;
 
 	// Use this for initialization
 	void Start () {
-	
+		Connect.getConnectHandle ((client) => {
+			if (client != null) {
+				_pclient = client;
+			} else {
+				Console.WriteLine("Error login.....");
+			}
+		});
 	}
 	
 	// Update is called once per frame
