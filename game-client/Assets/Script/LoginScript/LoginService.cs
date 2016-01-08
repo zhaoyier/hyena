368973baa595df767b01612340cf2477dce5ac09
zhaoyier <zjw_0911@126.com>
2016-01-08 18:50:56 +0800

    tt

--- game-client/Assets/Script/LoginScript/LoginService.cs
+++ game-client/Assets/Script/LoginScript/LoginService.cs
@@ -10,6 +10,8 @@ using UnityEngine.SceneManagement;
 public class LoginService : MonoBehaviour {
 	private static PomeloClient _pclient = null;
 
+	//public GameObject _btn = null;
+
 	// Use this for initialization
 	void Start () {
 		Connect.getConnectHandle ((client) => {
@@ -19,10 +21,27 @@ public class LoginService : MonoBehaviour {
 				Console.WriteLine("Error login.....");
 			}
 		});
+
+		//_btn = this.GetComponent<Game> ();
+		//UIEventListener.Get (_btn).onClick = OnClick2 ();
+	}
+
+	void Awake() {
+		//GameObject button = GameObject.FindGameObjectWithTag ("test");
+		GameObject button = GameObject.Find("UI Root (2D)/Camera/Anchor/Panel/Button");
+		UIEventListener.Get (button).onClick = ButtonClick;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void ButtonClick(GameObject button) {
+		Debug.Log ("GameObject" + button.name);
+	}
+
+	public void OnClick(GameObject obj){
+		Console.WriteLine ("111111111111111111");
 	}
 }
