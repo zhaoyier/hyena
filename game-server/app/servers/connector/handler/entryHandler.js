552c8ab5f224a7926aa94d5f8264cfde631fead3
zhaojianwei <zjw_0911@126.com>
2016-01-08 10:30:52 +0800

    update

--- game-server/app/servers/connector/handler/entryHandler.js
+++ game-server/app/servers/connector/handler/entryHandler.js
@@ -17,35 +17,7 @@ var handler = Handler.prototype;
  * @return {Void}
  */
 handler.enter = function(msg, session, next) {
-	var self = this;
-	var rid = msg.rid;
-	var uid = msg.username + '*' + rid
-	var sessionService = self.app.get('sessionService');
-
-	//duplicate log in
-	if( !! sessionService.getByUid(uid)) {
-		next(null, {
-			code: 500,
-			error: true
-		});
-		return;
-	}
-
-	session.bind(uid);
-	session.set('rid', rid);
-	session.push('rid', function(err) {
-		if(err) {
-			console.error('set rid for session service failed! error is : %j', err.stack);
-		}
-	});
-	session.on('closed', onUserLeave.bind(null, self.app));
-
-	//put user into channel
-	self.app.rpc.chat.chatRemote.add(session, uid, self.app.get('serverId'), rid, true, function(users){
-		next(null, {
-			users:users
-		});
-	});
+	next(null, {data: {username: 'admin', password: 'admin'}}});
 };
 
 /**
@@ -56,8 +28,5 @@ handler.enter = function(msg, session, next) {
  *
  */
 var onUserLeave = function(app, session) {
-	if(!session || !session.uid) {
-		return;
-	}
-	app.rpc.chat.chatRemote.kick(session, session.uid, app.get('serverId'), session.get('rid'), null);
+	
 };
