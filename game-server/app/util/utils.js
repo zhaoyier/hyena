5c9097f4519953f5951aa8858943be645af9d69b
zhaojianwei <zjw_0911@126.com>
2016-01-11 11:05:55 +0800

    update

---
+++ game-server/app/util/utils.js
@@ -0,0 +1,14 @@
+var utils = module.exports;
+
+// control variable of func "myPrint"
+var isPrintFlag = false;
+// var isPrintFlag = true;
+
+/**
+ * Check and invoke callback function
+ */
+utils.invokeCallback = function(cb) {
+  if(!!cb && typeof cb === 'function') {
+    cb.apply(null, Array.prototype.slice.call(arguments, 1));
+  }
+};
