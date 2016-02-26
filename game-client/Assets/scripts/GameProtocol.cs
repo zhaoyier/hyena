6ea3766a4fbd7939a9ced0a7ea34a6690149d211
zhaoyier <zjw_0911@126.com>
2016-02-26 19:01:55 +0800

    udpate

---
+++ game-client/Assets/scripts/GameProtocol.cs
@@ -0,0 +1,35 @@
+﻿using SimpleJson;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace GameMessage {
+	public class LoginRequest {
+		public string username { set; get;}
+		public string password { set; get;}
+
+		public JsonObject decode(JsonObject jsonObject) {
+			//SimpleJson.SimpleJson.SerializeObject ();
+		}
+
+		public JsonObject encode () {
+			
+		}
+
+
+	}
+
+	public class LoginResponse {
+		
+	}
+
+	public class RegisterRequest {
+		
+	}
+
+	public class RegisterResponse {
+		
+	}
+}
+//public class GameProtocol {
+
+//}
